781c962538a94de7b90a03098b199cc3a35d7b12
Lucas Ferreira <lucaasferreirafl@gmail.com>
2023-03-10 08:54:46 -0300

    feat: app setup

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { Knex, knex as setupKnex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './tmp/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
